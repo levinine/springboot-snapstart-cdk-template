0d413edb0489f0f44f3a04642f946721f91b7c47
Aleksandar <abuljevic8@gmail.com>
2023-05-24 12:02:06 +0200

    Now snapstart flag is set

--- cdk/src/main/java/com/levi9/celebrate9/CdkApp.java
+++ cdk/src/main/java/com/levi9/celebrate9/CdkApp.java
@@ -9,8 +9,9 @@ public class CdkApp {
 
         final String stage = System.getenv("STAGE");
 
-        if (stage == null || stage.equals(""))
+        if (stage == null || stage.equals("")) {
             throw new RuntimeException("Please ensure that the \"STAGE\" environment variable is set to a valid value.");
+        }
 
         final String stackName = "celebrate9-be-" + stage.toLowerCase();
         new CdkStack(app, stackName, stage, StackProps.builder()
--- cdk/src/main/java/com/levi9/celebrate9/CdkStack.java
+++ cdk/src/main/java/com/levi9/celebrate9/CdkStack.java
@@ -6,10 +6,10 @@ import software.amazon.awscdk.StackProps;
 import software.amazon.awscdk.services.apigateway.LambdaIntegration;
 import software.amazon.awscdk.services.apigateway.ProxyResourceOptions;
 import software.amazon.awscdk.services.apigateway.RestApi;
-import software.amazon.awscdk.services.lambda.Code;
-import software.amazon.awscdk.services.lambda.Function;
-import software.amazon.awscdk.services.lambda.FunctionProps;
+import software.amazon.awscdk.services.apigateway.RestApiProps;
+import software.amazon.awscdk.services.iam.ServicePrincipal;
 import software.amazon.awscdk.services.lambda.Runtime;
+import software.amazon.awscdk.services.lambda.*;
 import software.constructs.Construct;
 
 import java.io.File;
@@ -19,18 +19,29 @@ public class CdkStack extends Stack {
     public CdkStack(final Construct scope, final String id, final String stage, final StackProps props) {
         super(scope, id, props);
         initializeResources(stage);
+
     }
 
+
     private void initializeResources(final String stage) {
+
         final Function apiFunction = new Function(this, stage + "-api",
                 FunctionProps.builder()
-                        .functionName("api")
+                        .functionName(stage + "-api")
                         .runtime(Runtime.JAVA_17)
                         .code(Code.fromAsset(new File(new File(System.getProperty("user.dir")), "./api/target/api.jar").toString()))
                         .handler("com.levi9.celebrate9.StreamLambdaHandler")
                         .memorySize(1024)
                         .timeout(Duration.seconds(10))
                         .build());
+        apiFunction.grantInvoke(new ServicePrincipal("apigateway.amazonaws.com"));
+
+        final CfnFunction cfnFunction = (CfnFunction) apiFunction.getNode().getDefaultChild();
+        if (cfnFunction != null) {
+            cfnFunction.setSnapStart(CfnFunction.SnapStartProperty.builder()
+                    .applyOn("PublishedVersions")
+                    .build());
+        }
 
         final LambdaIntegration integration = new LambdaIntegration(apiFunction);
 
@@ -39,7 +50,9 @@ public class CdkStack extends Stack {
                 .defaultIntegration(integration)
                 .build();
 
-        final RestApi restApi = new RestApi(this, stage + "-RestApi");
+        final RestApi restApi = new RestApi(this, stage + "-RestApi", RestApiProps.builder()
+                .cloudWatchRole(true)
+                .build());
 
         restApi.getRoot().addProxy(proxyR);
     }
